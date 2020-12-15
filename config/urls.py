cf5479b86e2ca953152b0163bab51a67f3ae75c3
EverUnLOver <anewmeal@gmail.com>
2020-12-15 16:51:54 -0500

    prueba

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+"""Main URLs module."""
+
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+    # Django Admin
+    path(settings.ADMIN_URL, admin.site.urls),
+
+    path('', include(('identidad.personas.urls', 'personas'), namespace='personas')),
+
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ identidad/personas/apps.py
@@ -0,0 +1,11 @@
+"""App personas."""
+
+# Django
+from django.apps import AppConfig
+
+
+class PersonasConfig(AppConfig):
+    """Configuracion de app personas."""
+
+    name = 'identidad.personas'
+    verbose_name = 'Personas'
